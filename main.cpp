c2477c644d61489cd10b2c432c52c087cc3f6350
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2018-04-23 11:09:31 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include "findwords.cpp"
+
+using namespace std;
+
+int main(){
+cout<<endl;
+	for(int line=0;line<=14;line++){
+	for (int col=0;col<=14;col++){
+	cout<<" "<<wordlist[line][col];
+	}
+	cout<<endl;
+	}
+	cout<<endl;
+	cout<<"\t";cin>>n_input;
+	
+	findwords(n_input);
+}
